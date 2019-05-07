bb00abe3a641bb29fa2b68a3e7a5c2d838a61256
Stefan Isaia <thenyartist@gmail.com>
2019-05-07 01:24:21 +0000

    Form submits to DB

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,7 +5,18 @@ class QuotesController < ApplicationController
   end
 
   def new
-    @qupte = Quote.new
+    @quote = Quote.new
   end
+
+def create
+  Quote.create(quote_params)
+  redirect_to root_path
+end
+
+private
+
+def quote_params
+  params.require(:quote).permit(:saying, :author)
+end
 
 end
